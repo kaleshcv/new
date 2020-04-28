6328d23725ef5aaee4c8a70a814109e4df125a73
Kalesh <kaleshcv2@gmail.com>
2020-04-28 15:59:50 +0530

    testing

--- mongodb1.py
+++ mongodb1.py
@@ -8,7 +8,7 @@ dict={'name':'Kalesh', 'mobile':9744218219}
 mycol.insert_one(dict)
 
 
-#print(type(myclient))
-#print(type(mydb))
+print(type(myclient))
+print(type(mydb))
 dblist=myclient.list_database_names() # to list all db names
 #print(dblist)
